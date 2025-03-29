806ebcdc880640fae9a9005f8c647c519d965559
jin heesung <jinheesung@iMac.local>
2025-03-29 22:49:00 +0900

    add modelin toMap

---
+++ lib/models/user.dart
@@ -0,0 +1,31 @@
+class User {
+  final String id;
+  final String fullName;
+  final String email;
+  final String state;
+  final String city;
+  final String locality;
+  final String password;
+
+  User({
+    required this.id,
+    required this.fullName,
+    required this.email,
+    required this.state,
+    required this.city,
+    required this.locality,
+    required this.password,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "id": id,
+      "fullName": fullName,
+      "email": email,
+      "state": state,
+      "city": city,
+      "locality": locality,
+      "password": password,
+    };
+  }
+}
